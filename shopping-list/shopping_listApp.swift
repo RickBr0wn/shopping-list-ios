91f64e3b443a43a9b2ece28beb70232abfaf7c1a
Rick Brown <ricky.brown.00@gmail.com>
2021-10-13 14:19:16 +0100

    Initial Commit

---
+++ shopping-list/shopping_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_listApp.swift
+//  shopping-list
+//
+//  Created by Rick Brown on 13/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct shopping_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
